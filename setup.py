281dc47e01edf951bce1446417818d2438d6a9f7
Shreyas Kalvankar <42400733+obi-wan-shinobi@users.noreply.github.com>
2025-06-26 11:14:43 +0200

    Update setup.py to read version from version.txt

--- setup.py
+++ setup.py
@@ -2,3 +2,12 @@ from setuptools import setup, find_packages
 
 with open("version.txt") as f:
     version = f.read().strip()
+
+setup(
+    name="libversion",
+    version=version,
+    packages=find_packages(),
+    install_requires=[
+        "toml >= 0.10.2"
+    ],
+)
